b3849bc697796a13d4b9454ce3d1efd033cf7ada
phantamanta44 <evandalong@gmail.com>
2019-06-17 23:59:16 -0500

    Fix file extension detection failing for asset adapters

--- src/main/kotlin/xyz/phanta/wpd/renderer/adapter/AbstractFileTypeAdapter.kt
+++ src/main/kotlin/xyz/phanta/wpd/renderer/adapter/AbstractFileTypeAdapter.kt
@@ -5,6 +5,7 @@ import java.nio.file.Path
 
 abstract class AbstractFileTypeAdapter(private val mimetype: String, private vararg val extensions: String) : AssetAdapter {
 
-    override fun canParse(path: Path, mimetype: String?): Boolean = mimetype == this.mimetype || extensions.any { path.endsWith(it) }
+    override fun canParse(path: Path, mimetype: String?): Boolean =
+            mimetype == this.mimetype || extensions.any { path.fileName.toString().endsWith(it) }
 
 }
