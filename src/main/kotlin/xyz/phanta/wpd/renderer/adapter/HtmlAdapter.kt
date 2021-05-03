5c5de6da4aa65448b0dc065452815eb58fc39c3f
phantamanta44 <evandalong@gmail.com>
2021-05-02 20:05:02 -0500

    Use simple regex-based HTML parser instead of the weird WIRD one

--- src/main/kotlin/xyz/phanta/wpd/renderer/adapter/HtmlAdapter.kt
+++ src/main/kotlin/xyz/phanta/wpd/renderer/adapter/HtmlAdapter.kt
@@ -1,125 +1,128 @@
 package xyz.phanta.wpd.renderer.adapter
 
-import xyz.phanta.wird.parser.Grammar
-import xyz.phanta.wird.parser.Parser
-import xyz.phanta.wird.parser.ParserConfig
-import xyz.phanta.wird.parser.finalizer.Finalizers
-import xyz.phanta.wird.parsetree.ParseTreeParentNode
 import xyz.phanta.wpd.model.*
 import xyz.phanta.wpd.renderer.impl.*
 import xyz.phanta.wpd.util.ContextStackNode
-import xyz.phanta.wpd.util.loadGrammar
 import java.nio.charset.StandardCharsets
 
 class HtmlAdapter : AbstractFileTypeAdapter("text/html", ".html", ".htm", ".xhtml") {
 
     companion object {
 
-        private val GRAMMAR: Grammar = loadGrammar("/adapter/html.wird")
-        private val PARSER: Parser = GRAMMAR.newParser(
-            "html",
-            ParserConfig.Builder()
-                .withFinalizers("html", 0, Finalizers.flatten(0))
-                .withFinalizers("html_entity", 0, Finalizers.omit(0, 3))
-                .build()
-        )
+        private val TAG_PATTERN: Regex = Regex("""<\?(.)(?:\s*(.+?))?\s*\?>""")
 
     }
 
     override fun parse(key: String, data: ByteArray, deps: AssetResolver): Asset {
-        val astTokens = PARSER.parse(data.toString(StandardCharsets.UTF_8))
-        var contextStack = ContextStackNode(RenderableContextModel())
-        for (token in astTokens.subtrees) {
-            try {
-                when (token.bodyIndex) {
-                    0 -> {
-                        val op = token.getLeaf(0).content
-                        when (op) {
-                            "=" -> contextStack.children.add(StrictExpressionModel(token.operand))
-                            "~" -> contextStack.children.add(ExpressionModel(token.operand))
-                            ":" -> RenderableContextModel().let {
-                                contextStack.bindings[token.operand] = it
-                                contextStack = contextStack.push(it)
-                            }
-                            "." -> {
-                                val operands = token.operand.split("=", limit = 2)
-                                if (operands.size != 2) {
-                                    throw MalformationException("Malformed let-in expression!")
-                                }
-                                val letModel = ExpressionBindingContextModel(operands[0].trim(), operands[1].trim())
-                                contextStack.children.add(letModel)
-                                contextStack = contextStack.push(letModel)
-                            }
-                            "|" -> ConditionalContextModel(token.operand).let {
-                                contextStack.children.add(it)
-                                contextStack = contextStack.push(it)
-                            }
-                            "," -> {
-                                val condition = token.operand
-                                val newContext = if (condition.isEmpty()) {
-                                    RenderableContextModel()
-                                } else {
-                                    ConditionalContextModel(condition)
-                                }
-                                (contextStack.context as? ConditionalContextModel
-                                    ?: throw MalformationException("Conditional fallthrough block without conditional!"))
-                                    .fallthrough = newContext
-                                contextStack = contextStack.pop().push(newContext)
-                            }
-                            "[" -> {
-                                val operands = token.operand.split("<-", limit = 2)
-                                if (operands.size != 2) {
-                                    throw MalformationException("Malformed indexable for-each expression!")
-                                }
-                                val iterModel = IterationContextModel(operands[0].trim(), operands[1].trim())
-                                contextStack.children.add(iterModel)
-                                contextStack = contextStack.push(iterModel)
-                            }
-                            "{" -> {
-                                val operands = token.operand.split("<-", limit = 2)
-                                if (operands.size != 2) {
-                                    throw MalformationException("Malformed resolver data-import expression!")
-                                }
-                                val iterVars = operands[0].split(",", limit = 2)
-                                if (iterVars.size != 2) {
-                                    throw MalformationException("Malformed resolver data-import expression!")
-                                }
-                                val iterModel = ResolverIterationContextModel(
-                                    iterVars[0].trim(),
-                                    iterVars[1].trim(),
-                                    operands[1].trim()
-                                )
-                                contextStack.children.add(iterModel)
-                                contextStack = contextStack.push(iterModel)
-                            }
-                            "/" -> contextStack = contextStack.pop()
-                            "+" -> contextStack.children.add(ImportModel(token.operand))
-                            "d" -> {
-                                val operands = token.operand.split("=", limit = 2)
-                                if (operands.size != 2) {
-                                    throw MalformationException("Malformed data-import expression!")
-                                }
-                                val importModel = ImportContextModel(operands[0].trim(), operands[1].trim())
-                                contextStack.children.add(importModel)
-                                contextStack = contextStack.push(importModel)
-                            }
-                            else -> throw RenderingStateException("Invalid operator '$op'")
-                        }
-                    }
-                    1, 2 -> contextStack.children.add(RenderableLiteral(token.getLeaf(0).content))
-                    else -> throw IllegalStateException("Impossible state: ${token.bodyIndex}")
+        val dataStr = data.toString(StandardCharsets.UTF_8)
+        val wpdTags = TAG_PATTERN.findAll(dataStr)
+        try {
+            val parseState = ParseState()
+            var textStart = 0
+            for (tag in wpdTags) {
+                if (tag.range.first > textStart) {
+                    parseState.consumeText(dataStr.substring(textStart, tag.range.first))
                 }
-            } catch (e: RenderingException) {
-                throw AssetParsingException(key, e)
+                parseState.consumeWpdTag(tag.groupValues[1], tag.groupValues[2])
+                textStart = tag.range.last + 1
             }
+            if (textStart < dataStr.length) {
+                parseState.consumeText(dataStr.substring(textStart))
+            }
+            return parseState.finish(key)
+        } catch (e: RenderingException) {
+            throw AssetParsingException(key, e)
         }
-        if (contextStack.hasParent) {
-            throw RenderingStateException("Not at root context!")
-        }
-        return RenderableAsset(key, contextStack.context)
     }
 
-    private val ParseTreeParentNode.operand: String
-        get() = this.getLeaf(1).content.trim()
+    private class ParseState {
+
+        private var contextStack: ContextStackNode = ContextStackNode(RenderableContextModel())
+
+        fun consumeText(text: String) {
+            contextStack.children.add(RenderableLiteral(text))
+        }
+
+        fun consumeWpdTag(opcode: String, operand: String) {
+            when (opcode) {
+                "=" -> contextStack.children.add(StrictExpressionModel(operand))
+                "~" -> contextStack.children.add(ExpressionModel(operand))
+                ":" -> RenderableContextModel().let {
+                    contextStack.bindings[operand] = it
+                    contextStack = contextStack.push(it)
+                }
+                "." -> {
+                    val operands = operand.split("=", limit = 2)
+                    if (operands.size != 2) {
+                        throw MalformationException("Malformed let-in expression!")
+                    }
+                    val letModel = ExpressionBindingContextModel(operands[0].trimEnd(), operands[1].trimStart())
+                    contextStack.children.add(letModel)
+                    contextStack = contextStack.push(letModel)
+                }
+                "|" -> ConditionalContextModel(operand).let {
+                    contextStack.children.add(it)
+                    contextStack = contextStack.push(it)
+                }
+                "," -> {
+                    val newContext = if (operand.isEmpty()) {
+                        RenderableContextModel()
+                    } else {
+                        ConditionalContextModel(operand)
+                    }
+                    (contextStack.context as? ConditionalContextModel
+                        ?: throw MalformationException("Conditional fallthrough block without conditional!"))
+                        .fallthrough = newContext
+                    contextStack = contextStack.pop().push(newContext)
+                }
+                "[" -> {
+                    val operands = operand.split("<-", limit = 2)
+                    if (operands.size != 2) {
+                        throw MalformationException("Malformed indexable for-each expression!")
+                    }
+                    val iterModel = IterationContextModel(operands[0].trimEnd(), operands[1].trimStart())
+                    contextStack.children.add(iterModel)
+                    contextStack = contextStack.push(iterModel)
+                }
+                "{" -> {
+                    val operands = operand.split("<-", limit = 2)
+                    if (operands.size != 2) {
+                        throw MalformationException("Malformed resolver data-import expression!")
+                    }
+                    val iterVars = operands[0].split(",", limit = 2)
+                    if (iterVars.size != 2) {
+                        throw MalformationException("Malformed resolver data-import expression!")
+                    }
+                    val iterModel = ResolverIterationContextModel(
+                        iterVars[0].trimEnd(),
+                        iterVars[1].trim(),
+                        operands[1].trimStart()
+                    )
+                    contextStack.children.add(iterModel)
+                    contextStack = contextStack.push(iterModel)
+                }
+                "/" -> contextStack = contextStack.pop()
+                "+" -> contextStack.children.add(ImportModel(operand))
+                "d" -> {
+                    val operands = operand.split("=", limit = 2)
+                    if (operands.size != 2) {
+                        throw MalformationException("Malformed data-import expression!")
+                    }
+                    val importModel = ImportContextModel(operands[0].trimEnd(), operands[1].trimStart())
+                    contextStack.children.add(importModel)
+                    contextStack = contextStack.push(importModel)
+                }
+                else -> throw RenderingStateException("Invalid operator '$opcode'")
+            }
+        }
+
+        fun finish(key: String): RenderableAsset {
+            if (contextStack.hasParent) {
+                throw RenderingStateException("Not at root context!")
+            }
+            return RenderableAsset(key, contextStack.context)
+        }
+
+    }
 
 }
